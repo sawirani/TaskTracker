cf64b6d8e9907c60a9221dae2c9ee54e0654f633
Anastasiia Grishina <Anastasiia Grishina>
2020-12-19 11:24:59 +0300

    Add tasks comments

--- src/main/ui/src/app/_services/task.service.ts
+++ src/main/ui/src/app/_services/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
 const baseUrl = 'http://localhost:8080/api/tasks';
 
@@ -8,6 +9,29 @@ const baseUrl = 'http://localhost:8080/api/tasks';
   providedIn: 'root'
 })
 export class TaskService {
+
+  tasksCommentStub = [
+    {
+      userName: 'Name',
+      comment: 'Text',
+      date: '12.19.2020'
+    },
+    {
+      userName: 'Name',
+      comment: 'Text',
+      date: '12.19.2020' 
+    },
+    {
+      userName: 'Name',
+      comment: 'Text',
+      date: '12.19.2020' 
+    },
+    {
+      userName: 'Name',
+      comment: 'Text',
+      date: '12.19.2020' 
+    }
+  ]
 
   constructor(private http: HttpClient) { }
 
@@ -38,4 +62,19 @@ export class TaskService {
   findByTitle(taskTitle): Observable<any> {
     return this.http.get(`${baseUrl}?taskTitle=${taskTitle}`);
   }
+
+  getTaskComments(task): Observable<any> {
+    // TODO add api
+    // return this.http.get()
+    return of(this.tasksCommentStub);
+  }
+
+  addTaskComments(comment): Observable<any> {
+     // TODO add api
+    // return this.http.get()
+    this.tasksCommentStub.push(comment);
+    return of(this.tasksCommentStub);
+  }
+
+  
 }
--- src/main/ui/src/app/components/task-list/tasks-list.component.ts
+++ src/main/ui/src/app/components/task-list/tasks-list.component.ts
@@ -19,6 +19,12 @@ export class TasksListComponent implements OnInit {
   showAdminBoard = false;
   showModeratorBoard = false;
 
+  form = {
+    comment: ''
+  }
+  user;
+  comments;
+
   constructor(private taskService: TaskService,
               private tokenStorageService: TokenStorageService) { }
 
@@ -26,9 +32,9 @@ export class TasksListComponent implements OnInit {
     this.retrieveTasks();
 
     if (!!this.tokenStorageService.getToken()) {
-      const user = this.tokenStorageService.getUser();
-      this.showAdminBoard = user.roles.includes('ROLE_ADMIN');
-      this.showModeratorBoard = user.roles.includes('ROLE_MODERATOR');
+      this.user = this.tokenStorageService.getUser();
+      this.showAdminBoard = this.user.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.user.roles.includes('ROLE_MODERATOR');
     }
   }
 
@@ -53,6 +59,7 @@ export class TasksListComponent implements OnInit {
   setActiveTask(task, index): void {
     this.currentTask = task;
     this.currentIndex = index;
+    this.getTaskComments();
   }
 
   removeAllTasks(): void {
@@ -82,6 +89,29 @@ export class TasksListComponent implements OnInit {
   onTaskCreate(): void{
     this.retrieveTasks();
     this.isTaskEditMode = false;
+  }
+
+  addComment(): void{
+    const comment = {
+      taskId: this.currentTask.id,
+      userName: this.user.id,
+      comment: this.form.comment,
+      date: new Date() 
+    }
+
+    this.taskService.addTaskComments(comment).subscribe(()=>{
+      this.form.comment = '';
+      this.getTaskComments();
+    })
+  }
+
+  getTaskComments(): void {
+    this.taskService.getTaskComments(this.currentTask.id).subscribe( comments =>{
+      this.comments = comments.map( value => {
+        value.date = new Date(value.date);
+        return value;
+      })
+    })
   }
 
 }
